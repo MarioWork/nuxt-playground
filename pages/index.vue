3fbc7aa8268f2d542dab54475d7d60c985db9156
MarioWork <mariovieira.work@hotmail.com>
2023-03-24 21:31:35 -0100

    project init

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Page 1</h1>
+    <NuxtLink to="">Next Page</NuxtLink>
+    <ul>
+      <li v-for="{ id, name } in people" :key="id">{{ id }} - {{ name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+const people = [
+  { id: 1, name: "John" },
+  { id: 2, name: "Joseph" },
+  { id: 3, name: "Lily" },
+];
+
+export default {
+  data() {
+    return {
+      people: people,
+    };
+  },
+};
+</script>
