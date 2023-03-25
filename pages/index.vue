89dab6ab51e15c66cbdfe3353600634657d83b43
MarioWork <mariovieira.work@hotmail.com>
2023-03-25 01:58:07 -0100

    fetch users

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,30 @@
 <template>
   <div>
-    <h1>Page 1</h1>
-    <NuxtLink to="">Next Page</NuxtLink>
+    <h1>Users</h1>
+    <button @click="nextPage">next</button>
     <ul>
-      <li v-for="{ id, name } in people" :key="id">{{ id }} - {{ name }}</li>
+      <li v-for="{ id, first_name } in users" :key="id">
+        {{ id }} - {{ first_name }}
+      </li>
     </ul>
   </div>
 </template>
 
-<script>
-const people = [
-  { id: 1, name: "John" },
-  { id: 2, name: "Joseph" },
-  { id: 3, name: "Lily" },
-];
+<script setup>
+const page = ref(1);
+const users = ref([]);
 
-export default {
-  data() {
-    return {
-      people: people,
-    };
-  },
+const getUsers = async (page) => {
+  const { data } = await useFetch(
+    () => `https://reqres.in/api/users?page=${page}`
+  );
+  return data.value.data;
+};
+
+users.value = await getUsers(page.value);
+
+const nextPage = async () => {
+  page.value++;
+  users.value = await getUsers(page.value);
 };
 </script>
