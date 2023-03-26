e6f4d0d263010333e0c427d0f7b62bc90560885d
MarioWork <mariovieira.work@hotmail.com>
2023-03-26 21:27:14 +0000

    add meta data

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,18 @@
 export default defineNuxtConfig({
   // ...
   runtimeConfig: {
+    app: {
+      head: {
+        title: "Nuxt 3 Playground",
+        meta: [
+          {
+            name: "description",
+            content:
+              "This project has the objective of helping to learn vue 3 ",
+          },
+        ],
+      },
+    },
     public: {
       baseURL: "https://reqres.in/api",
     },
