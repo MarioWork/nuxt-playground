56e03285fb475ed199a4dc2207794dfd4191a34c
MarioWork <mariovieira.work@hotmail.com>
2023-03-26 21:24:23 +0000

    create a custom error

---
+++ error.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>
+      {{ error.statusCode }}
+    </h1>
+    <h4>{{ error.message }}</h4>
+  </div>
+</template>
+
+<script setup>
+defineProps(["error"]);
+</script>
+
+<style scoped>
+div {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
