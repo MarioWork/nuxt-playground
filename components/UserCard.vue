cf69e313e93d9d320c39afb6a0a6911c09e99d8a
MarioWork <mariovieira.work@hotmail.com>
2023-03-28 16:05:23 +0000

    create user card

---
+++ components/UserCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="main">
+    <NuxtLink>
+      <div id="card">
+        <img :src="`${user.avatar}`" alt="" />
+        <h1>{{ user.first_name }}</h1>
+        <h1>{{ user.last_name }}</h1>
+      </div>
+    </NuxtLink>
+  </div>
+</template>
+
+<script setup>
+const { user } = defineProps({
+  user: { type: Object, required: true },
+});
+</script>
+
+<style scoped>
+#main {
+  width: 100%;
+  padding: 0.5em;
+  display: flex;
+  background-color: #f2e5e5;
+  border-radius: 25px;
+  padding: 0.5em;
+}
+
+#card {
+  display: flex;
+}
+
+img {
+  border-radius: 50%;
+}
+</style>
