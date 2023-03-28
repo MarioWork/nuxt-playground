4d1cfc09a12a267423082336240a93694d7c0bc0
MarioWork <mariovieira.work@hotmail.com>
2023-03-28 14:54:55 +0000

    fetch user using none setup script

--- pages/user/[id].vue
+++ pages/user/[id].vue
@@ -1,12 +1,37 @@
 <template>
-  <div>{{ id }}</div>
+  <div>{{ user }}</div>
 </template>
 
 <script>
-//Options API Example
+//No script setup example
 export default {
-  mounted() {
-    console.log(this.$route.params);
+  async setup() {
+    const config = useRuntimeConfig();
+
+    const { id } = useRoute().params;
+
+    if (isNaN(id))
+      throw createError({
+        statusCode: 404,
+        message: "Id needs to be a number",
+      });
+
+    const getUser = async () => {
+      const { data } = await useFetch(() => `/users/${id}`, {
+        baseURL: config.baseURL,
+      });
+
+      const { data: user } = data.value;
+
+      if (!user)
+        throw createError({ statusCode: 404, message: "User not found" });
+
+      return user;
+    };
+
+    return {
+      user: await getUser(),
+    };
   },
 };
 </script>
