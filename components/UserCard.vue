f09c59cab4579891a8bdb7634dee6443ed7a213c
MarioWork <mariovieira.work@hotmail.com>
2023-03-29 23:21:31 +0000

    better styling

--- components/UserCard.vue
+++ components/UserCard.vue
@@ -1,13 +1,17 @@
 <template>
-  <div id="main">
+  <article id="main">
     <NuxtLink :to="`/user/${user.id}`">
-      <div id="card">
+      <main id="card">
         <img :src="`${user.avatar}`" alt="Profile Picture" loading="lazy" />
-        <h1>{{ user.first_name }}</h1>
-        <h3>{{ user.last_name }}</h3>
-      </div>
+        <section>
+          <label>First Name</label>
+          <h1>{{ user.first_name }}</h1>
+          <label>Last Name</label>
+          <h2>{{ user.last_name }}</h2>
+        </section>
+      </main>
     </NuxtLink>
-  </div>
+  </article>
 </template>
 
 <script setup>
@@ -19,6 +23,11 @@
 </script>
 
 <style scoped>
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
   #main {
     width: 80%;
     padding: 0.5em;
@@ -26,15 +35,26 @@
     background-color: #f2e5e5;
     border-radius: 25px;
     cursor: pointer;
+    transition-duration: 0.5s;
+  }
+
+  #main:hover {
+    -webkit-box-shadow: 0px 0px 50px 5px rgba(0, 0, 0, 0.65);
+    -moz-box-shadow: 0px 0px 50px 5px rgba(0, 0, 0, 0.65);
+    box-shadow: 0px 0px 50px 5px rgba(0, 0, 0, 0.65);
+    scale: 1.1;
   }
 
   #card {
     display: flex;
+    align-items: center;
+    gap: 1em;
   }
 
   a {
     color: black;
     text-decoration: none;
+    width: 100%;
   }
 
   img {
@@ -42,5 +62,17 @@
     height: 150px;
     width: 150px;
     object-fit: cover;
+  }
+
+  section {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 0.5em;
+  }
+
+  h1 {
+    font-size: 2em;
   }
 </style>
