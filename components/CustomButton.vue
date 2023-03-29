7c386b6df3841e0ddd2c1710af73087440896e25
MarioWork <mariovieira.work@hotmail.com>
2023-03-29 23:21:24 +0000

    fix indentation

--- components/CustomButton.vue
+++ components/CustomButton.vue
@@ -5,33 +5,33 @@
 </template>
 
 <script setup>
-const props = defineProps({
-  onClick: {
-    type: Function,
-    default: () => {},
-  },
-});
+  const props = defineProps({
+    onClick: {
+      type: Function,
+      default: () => {},
+    },
+  });
 
-const { onClick } = toRefs(props);
+  const { onClick } = toRefs(props);
 </script>
 
 <style scoped>
-button {
-  width: 250px;
-  height: 45px;
-  padding: 0.5em 1.5em;
-  border: none;
-  border-radius: 25px;
-  font-size: 1em;
-  font-weight: bold;
-  background-color: #ce7777;
-  color: white;
-  cursor: pointer;
-  transition-duration: 0.5s;
-}
+  button {
+    width: 250px;
+    height: 45px;
+    padding: 0.5em 1.5em;
+    border: none;
+    border-radius: 25px;
+    font-size: 1em;
+    font-weight: bold;
+    background-color: #ce7777;
+    color: white;
+    cursor: pointer;
+    transition-duration: 0.5s;
+  }
 
-button:hover {
-  opacity: 0.5;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-}
+  button:hover {
+    opacity: 0.5;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  }
 </style>
