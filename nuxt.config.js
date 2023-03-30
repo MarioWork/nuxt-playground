89082d17197b30356ad6707250d3861481abb4f6
MarioWork <mariovieira.work@hotmail.com>
2023-03-30 00:42:57 +0000

    fix a bug where, app definition was inside runtimeConfig

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,19 +1,24 @@
 import { defineNuxtConfig } from 'nuxt/config';
 
 export default defineNuxtConfig({
-  runtimeConfig: {
-    app: {
-      head: {
-        title: 'Nuxt 3 Playground',
-        meta: [
-          {
-            name: 'description',
-            content:
-              'This project has the objective of helping to learn vue 3 ',
-          },
-        ],
-      },
+  app: {
+    head: {
+      title: 'Nuxt 3 Playground',
+      meta: [
+        {
+          name: 'description',
+          content: 'This project has the objective of helping to learn vue 3 ',
+        },
+      ],
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/icon?family=Material+Icons',
+        },
+      ],
     },
+  },
+  runtimeConfig: {
     public: {
       baseURL: 'https://reqres.in/api',
     },
