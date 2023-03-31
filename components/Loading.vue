f426354a79dac5a37be54f62783eb113cbd3df91
MarioWork <mariovieira.work@hotmail.com>
2023-03-31 18:42:09 +0000

    create a loading component

---
+++ components/Loading.vue
@@ -0,0 +1,26 @@
+<template>
+  <main>
+    <img
+      src="https://media.tenor.com/wpSo-8CrXqUAAAAi/loading-loading-forever.gif"
+      alt="loading"
+    />
+  </main>
+</template>
+
+<script setup></script>
+
+<style scoped>
+  main {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(133, 133, 133, 0.426);
+  }
+
+  img {
+    width: 5%;
+    height: 5%;
+    object-fit: contain;
+  }
+</style>
